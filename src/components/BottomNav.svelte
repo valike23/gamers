b1b5f3ac4ac1ba651ae1613e5ee3339b564d69dd
valike23 <ludike23@gmail.com>
2021-11-06 10:04:35 +0100

    fix the team tab

--- src/components/BottomNav.svelte
+++ src/components/BottomNav.svelte
@@ -1,18 +1,15 @@
 
-<div class="bottom-nav pos-absolute">
-    <button class="button">
+<div class="bottom-nav pos-absolute ">
+    <button class="button bg-amber fg-white">
         <span class="fa fa-soccer-ball-o"></span>
         <span class="label">Matches</span>
     </button>
-    <button class="button">
-        <span class="icon mif-favorite"></span>
+   
+    <button class="button bg-amber fg-white">
+        <span class="fa fa-users"></span>
         <span class="label">Squad</span>
     </button>
-    <button class="button">
-        <span class="icon mif-search"></span>
-        <span class="label">Friends</span>
-    </button>
-    <button class="button">
+    <button class="button bg-amber fg-white">
         <span class="icon mif-search"></span>
         <span class="label">LeaderBoard</span>
     </button>
